ea7f6baecfd2bf97cfc246f2eeff512fbf3bf1c9
Pawel Zienkiewicz <zienekp@gmail.com>
2013-09-10 14:24:09 +0200

    ...

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,6 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
-# http://www.simonecarletti.com/blog/2012/02/heroku-and-rails-3-2-assetprecompile-error/
-config.assets.initialize_on_precompile = false
-
 module Pagepimp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
